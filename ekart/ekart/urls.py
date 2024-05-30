0ebe2b282bc4c7e438d489cfa49226b6c119e0ff
Nishand kp <nishandhkp@gmail.com>
2024-05-31 00:29:32 +0530

    second edit

--- ekart/ekart/urls.py
+++ ekart/ekart/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from ekart_app import views
 from orders import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('order/',views.print_hello)
+    path('order/',views.print_hello),
+    path('',include('products.urls')),
 
-]
+ ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
